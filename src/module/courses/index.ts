479351882ff655f6a81fc0d2bb2fff031674dd94
Hammam Hussein <hamamhussein10@gmail.com>
2026-04-22 19:44:32 +0300

    Enhance lesson module

--- src/module/courses/index.ts
+++ src/module/courses/index.ts
@@ -6,3 +6,6 @@ export { ICOURSE_REPOSITORY } from './domain/constants/injection.token';
 
 // export interface repo
 export type { ICourseRepository } from './domain/repository/course.repository.interface';
+
+// export handler
+export { CoursePurchasedEvent } from './domain/events/course-purchased.event';
