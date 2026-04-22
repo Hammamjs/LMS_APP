8d05539c968a1a3834e094d7aefe3b1382ae95a4
Hammam Hussein <hamamhussein10@gmail.com>
2026-04-22 19:12:20 +0300

    add notificaion sys

---
+++ src/module/notification/domain/repository/notification.repository.interface.ts
@@ -0,0 +1,21 @@
+import { PaginationResult, Result } from '@core/index';
+import { NotificationPaginationResult } from '../entity/notification.type';
+import { Notification } from '../entity/notification.entity';
+
+export interface INotificationSystemRepository {
+  findAll: (
+    params: NotificationPaginationResult,
+  ) => Promise<Result<PaginationResult<Notification>>>;
+
+  findById: (id: string) => Promise<Result<Notification>>;
+
+  // update all messages as read
+  markAllAsRead: (ids: string[]) => Promise<Result<void>>;
+
+  save: (notification: Notification) => Promise<Result<Notification>>;
+  // soft deletion
+  delete: (id: string) => Promise<Result<void>>;
+
+  // delete all messages
+  deleteAll: (ids: string[]) => Promise<Result<void>>;
+}
